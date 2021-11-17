86c3532666be76ed2899f94a00da9c036d220596
Sushil Kumar Mahato <suloksushil@gmail.com>
2021-11-17 11:27:17 +0530

    Update Textform.js
    1 .made changes with the word counter

--- src/components/Textform.js
+++ src/components/Textform.js
@@ -40,14 +40,14 @@ export default function Textform(props) {
         <textarea className = "form-control" value={text}  style={{backgroundColor:props.mode==='dark'?'grey':'white', color:props.mode==='dark'?'white':'#04020d' }} onChange={handleOnChange} id = "exampleFormControlTextarea1" rows = "8" > </textarea> 
         </div>  
         <button className = "btn btn-primary mx-2  my-1" onClick={handleUpClick} > Convert to Uppercase </button> 
-        <button className = "btn btn-primary" onClick={handleloClick} > Convert to Lowercase </button> 
+        <button className = "btn btn-primary mx-2 my-1" onClick={handleloClick} > Convert to Lowercase </button> 
         <button className = "btn btn-primary mx-2 my-1" onClick={handlespaces}  > Trim sentence</button> 
 
         </div>
 
         <div className="container my-4"  style={{color:props.mode==='dark'?'white':'#04020d'}} >
             <h2>Your text summary</h2>
-            <p>{text.split(" ").length } words and {text.length} characters</p>
+            <p>{text.split(" ").filter((element)=>{return element.length!=0}).length}  words and {text.length} characters</p>
             <p>{ 0.008 * text.split(" ").length} minutes read</p>
             <h2>Preview</h2>
             <p>{text.length>0?text:"Enter something to preview"}</p>
